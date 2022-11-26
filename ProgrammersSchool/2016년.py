f9f5f0deabe2143c44510ac20fc0eb1d47627638
WonJoonChoi <724thomas@gmail.com>
2022-11-26 18:32:07 +0900

    221126

--- ProgrammersSchool/2016년.py
+++ ProgrammersSchool/2016년.py
@@ -5,3 +5,9 @@ def solution(a, b):
     adict={0:"MON",1:"TUE",2:"WED",3:"THU",4:"FRI",5:"SAT",6:"SUN"}
     x=datetime.datetime(2016,a,b)
     return adict[x.weekday()]
+
+import datetime
+def solution(a, b):
+    adict={0:"MON",1:"TUE",2:"WED",3:"THU",4:"FRI",5:"SAT",6:"SUN"}
+    x=datetime.datetime(2016,a,b)
+    return adict[x.weekday()]
