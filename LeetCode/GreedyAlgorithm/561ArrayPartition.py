fb8032c38207ecd2f3523b2571004acac849dfdf
ThomasChoi <724thomas@gmail.com>
2022-11-29 13:13:09 +0900

    221129

---
+++ LeetCode/GreedyAlgorithm/561ArrayPartition.py
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/array-partition/
+
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        ans=0
+        nums=sorted(nums)
+        for i in range(0,len(nums),2):
+            ans+=nums[i]
+        return ans
