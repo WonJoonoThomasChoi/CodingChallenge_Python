b817cd056d3c7643249c00c763f813be82b60b26
WonJoonChoi <724thomas@gmail.com>
2022-12-04 23:30:34 +0900

    221204

--- LeetCode/GreedyAlgorithm/1974MinimumTimeToTypeWordUsingSpecialTypewriter.py
+++ LeetCode/GreedyAlgorithm/1974MinimumTimeToTypeWordUsingSpecialTypewriter.py
@@ -1,4 +1,4 @@
-#https://leetcode.com/problems/minimum-time-to-type-word-using-special-typewriter/
+##https://leetcode.com/problems/minimum-time-to-type-word-using-special-typewriter/
 
 class Solution:
     def minTimeToType(self, word: str) -> int:
