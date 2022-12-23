4c6acf30f58444325743fd362f92e56fdedd57a9
WonJoonChoi <724thomas@gmail.com>
2022-12-23 21:51:40 +0900

    221223

---
+++ baekjoon/9625.py
@@ -0,0 +1,13 @@
+#https://www.acmicpc.net/problem/9625
+
+import sys
+n = int(sys.stdin.readline())
+adict = {}
+def fib(n):
+    if n in adict:
+        return adict[n]
+    else:
+        adict[n] = n-1 if n<=2 else fib(n - 1) + fib(n - 2)
+        return adict[n]
+print(fib(n),fib(n+1))
+
