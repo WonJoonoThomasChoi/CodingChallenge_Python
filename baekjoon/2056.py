2105261c179ddf380ad3eb9aef7e046568506bb1
ThomasChoi <724thomas@gmail.com>
2022-12-27 14:37:33 +0900

    221227

--- baekjoon/2056.py
+++ baekjoon/2056.py
@@ -1,13 +1,18 @@
 #https://www.acmicpc.net/problem/2056
-'''
 
-1. 5 0 = 0~5
-2. 1 1 1 = 5+1 = 5~6
-3. 3 1 2  = 5+1+3 = 6~9
-4. 6 1 1 = 5+6 = 5~11
-5. 1 2 2 4 = 6 + 11 + 1 = 18
-6. 8 2 2 4 = 6 + 11 + 8 = 25
-7. 4 3 3 5 6 = 9 + 18 + 25 + 4 = 56
+ntasks = int(input())
+dp={}
 
-'''
+nums = list(map(int, input().split()))
+dp[1] = nums[0]
+
+for i in range(ntasks-1):
+    nums = list(map(int, input().split()))
+    time = nums[0]
+    ntask = nums[1]
+    pretask=[]
+    if ntask!=0:
+        time = time + max([ dp[x] for x in nums[2::]])
+    dp[i+2] = time
+print(max(dp.values()))
 
