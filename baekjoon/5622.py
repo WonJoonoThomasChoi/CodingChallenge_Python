9de3d6b579c3534188a40f492dac66de144863af
WonJoonChoi <724thomas@gmail.com>
2022-12-29 23:00:48 +0900

    221229

--- baekjoon/5622.py
+++ baekjoon/5622.py
@@ -1,7 +1,7 @@
-#https://www.acmicpc.net/problem/5622
+# https://www.acmicpc.net/problem/5622
 
-adict={"A":2, "B":2, "C":2, "D":3, "E":3, "F":3, "G":4, "H":4, "I":4, "J":5, "K":5, "L":5, "M":6, "N":6, "O":6, "P":7, "Q":7, "R":7, "S":7, "T":8, "U":8, "V":8, "W":9, "X":9, "Y":9, "Z":9 }
-ans=0
-for i in input():
-    ans+=adict[i]+1
-print(ans)
+alst = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+letters = input()
+for i in range(len(alst)):
+    letters = letters.replace(alst[i], str(i))
+print(len(letters))
