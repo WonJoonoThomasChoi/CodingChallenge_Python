af8f2c65114f42b84f1235ffedc94768e346e747
WonJoonChoi <724thomas@gmail.com>
2022-12-30 21:44:51 +0900

    221230

---
+++ baekjoon/11653.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/11653
+
+num = int(input())
+
+while num!=1:
+    for i in range(2, num+1):
+        if num%i==0:
+            print(i)
+            num = num//i
+            break
