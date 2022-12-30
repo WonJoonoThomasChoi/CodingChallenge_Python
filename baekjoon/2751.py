4b8ebaaa6be2f1512959322daea60128c6fe7293
WonJoonChoi <724thomas@gmail.com>
2022-12-31 00:56:05 +0900

    221231

---
+++ baekjoon/2751.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/2751
+
+import sys
+num_list = []
+for _ in range(int(sys.stdin.readline())):
+    num_list.append(int(sys.stdin.readline()))
+num_list.sort()
+for i in num_list:
+    print(i)
