021654ec9b291b6e76c8a0679382ef70a531440f
WonJoonChoi <724thomas@gmail.com>
2022-12-30 21:45:01 +0900

    221230

---
+++ LeetCode/Practice.py
@@ -0,0 +1,17 @@
+# nums = list(map(int, input().split()))
+# num=int(input())
+# h, w, n = map(int, input().split())
+
+def primenumber(x):
+    if x==1:
+        return False
+    for i in range(2, int(x ** (1 / 2) + 1)):
+        if x % i == 0:
+            return False
+    return True
+
+
+Cases = int(input())
+nums = list(map(int, input().split()))
+print( sum([primenumber(x) for x in nums]) )
+
