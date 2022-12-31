81cc684549a439afa37b24fd51fa11a9000c4dfe
WonJoonChoi <724thomas@gmail.com>
2023-01-01 05:18:22 +0900

    230101

---
+++ baekjoon/1436.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/1436
+
+import sys
+n = int(sys.stdin.readline())
+count = 0
+num = 666
+while True:
+    if '666' in str(num):
+        count+=1
+        if count == n:
+            print(num)
+            break
+    num+=1
+
