bf861f157026977577fad5c32445fd3975eb9329
ThomasChoi <724thomas@gmail.com>
2023-01-02 19:35:19 +0900

    230102

---
+++ baekjoon/0Practice.py
@@ -0,0 +1,8 @@
+import numpy
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+for i in range(len(matrix)):
+    for j in range(i+1, len(matrix[i])):
+        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+print(matrix)
