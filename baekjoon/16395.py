7a03c7cf78d3245bb2eab517ceb0440cb2e9aff4
WonJoonChoi <724thomas@gmail.com>
2023-01-06 01:52:22 +0900

    230106

--- baekjoon/16395.py
+++ baekjoon/16395.py
@@ -3,14 +3,12 @@
 import sys
 n, k = map(int, sys.stdin.readline().split())
 dp=[0]*(n+1)
-dp[1]=1
+dp[0]=1
 
-
-
-for i in range(2,n+1):
-    for j in range(1,i):
-        dp[i]+=dp[j]
-
-print(dp[n])
-
+for j in range(n):
+    temp = [0] * (n+1)
+    for i in range(1,j+2):
+        temp[i] = dp[i-1] + dp[i]
+    dp = temp.copy()
+print(dp[k])
 
