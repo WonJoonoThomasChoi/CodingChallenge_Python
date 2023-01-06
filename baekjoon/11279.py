4908ee4c3ca42009de8a250eb44567268af685b0
WonJoonChoi <724thomas@gmail.com>
2023-01-07 02:03:06 +0900

    2301077

--- baekjoon/11279.py
+++ baekjoon/11279.py
@@ -2,15 +2,14 @@
 
 import sys
 import heapq
-n = int(sys.stdin.readline())
 heap = []
-for j in range(n):
-    temp = int(sys.stdin.readline().replace("\n", ""))
-    if temp != 0:
+for j in range(int(sys.stdin.readline())):
+    temp = int(sys.stdin.readline())
+    if temp!=0:
         heapq.heappush(heap, -temp)
-    else:
-        print(0) if len(heap) == 0 else print(-heap[0])
-        try:
-            heapq.heappop(heap)
-        except:
-            continue
+    else :
+        if len(heap)!=0:
+            print(-heapq.heappop(heap))
+        else:
+            print(0)
+
