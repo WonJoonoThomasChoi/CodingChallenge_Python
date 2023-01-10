cb02854c62838aeaf9a88559b243b7aada840a8a
WonJoonChoi <724thomas@gmail.com>
2023-01-11 07:56:58 +0900

    230111

--- baekjoon/1920.py
+++ baekjoon/1920.py
@@ -1,5 +1,19 @@
 # https://www.acmicpc.net/problem/1920
 
+'''
+1. 아이디어 :
+    1) 이진탐색을 이용
+    2) 중복을 제거해주는 set()을 이용
+2. 시간복잡도 :
+    1) 이진탐색 -  O(nlogn) + O(mlogn)= O(nlogn)
+    - n : n개의 원소를 Sorting에 걸리는 시간 + m : m번 탐색에 걸리는시간
+    2 ) set() - O(n) + O(m) = O(n)
+    - n : n개에 원소를 set에 넣는데 걸리는 시간 (1) + m : m개의 원소를 set에서 찾는데 걸리는 시간 (1)
+3. 자료구조 :
+    1) 이진탐색
+    2) set()
+'''
+
 import sys
 numbers = set()
 noneed1, save_numbers, noneed2, check_numbers = input(), list(map(int, sys.stdin.readline().split())), input(), list(map(int, sys.stdin.readline().split()))
