46b6298f924e450410391864e4f8ae7b31571195
WonJoonChoi <724thomas@gmail.com>
2023-01-11 07:56:44 +0900

    230111

--- baekjoon/10828.py
+++ baekjoon/10828.py
@@ -1,22 +1,26 @@
-#https://www.acmicpc.net/problem/10828
+# https://www.acmicpc.net/problem/10828
+
+'''
+1. 아이디어 :
+    1) 스택을 사용하여 명령어를 처리.
+2. 시간복잡도 :
+    1) O(n) : n : 입력받은 명령어의 개수
+3. 자료구조 :
+    1) 스택
+'''
 
 import sys
-n = int(sys.stdin.readline())
-ans = []
+
+n, ans = int(sys.stdin.readline()), []
 for j in range(n):
-    i=(sys.stdin.readline().replace("\n", "").split(" "))
+    i = list(sys.stdin.readline().split())
     if i[0] == "push":
         ans.append(int(i[1]))
     elif i[0] == "pop":
-        if len(ans) == 0:
-            print(-1)
-        else:
-            print(ans[-1])
-            ans = ans[:-1]
-    elif i[0]=="size":
+        print(-1 if len(ans) == 0 else ans.pop())
+    elif i[0] == "size":
         print(len(ans))
-    elif i[0]=="empty":
-        print(0 if len(ans)!=0 else 1)
-    elif i[0]=="top":
-        print(-1 if len(ans)==0 else ans[-1])
-
+    elif i[0] == "empty":
+        print(0 if ans else 1)
+    elif i[0] == "top":
+        print(ans[-1] if ans else -1)
