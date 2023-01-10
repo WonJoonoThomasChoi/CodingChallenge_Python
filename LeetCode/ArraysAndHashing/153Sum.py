9a4c5ba8291d542faa456c3c8987188a3c891ec1
WonJoonChoi <724thomas@gmail.com>
2023-01-11 07:56:19 +0900

    230111

--- LeetCode/ArraysAndHashing/153Sum.py
+++ LeetCode/ArraysAndHashing/153Sum.py
@@ -1,5 +1,13 @@
 #https://leetcode.com/problems/3sum/description/
-
+'''
+1. 아이디어 :
+    1) for문으로 num1을 target으로 지정하고, 나머지를 투 포인터로 확인한다.
+2. 시간복잡도 :
+    1) O(n) * O(nlogn) = O(n^2 * logn)
+    - num1타겟을 for문으로 돌고((n) * 나머지를 투포인터로 돌고(n) * 정렬(logn)
+3. 자료구조 :
+    1) 배열
+'''
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         numset = sorted(nums)
