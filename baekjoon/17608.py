a81c1ff76db798b5209f9ae75a7e60deebe06fc5
WonJoonChoi <724thomas@gmail.com>
2023-01-11 07:57:12 +0900

    230111

--- baekjoon/17608.py
+++ baekjoon/17608.py
@@ -1,5 +1,13 @@
 # https://www.acmicpc.net/problem/17608
-
+'''
+1. 아이디어 :
+    1) 배열을 이용하여 current max값 추적
+2. 시간복잡도 :
+    1) O(n)
+    - for문을 돌면서 조건문 확인
+3. 자료구조 :
+    1) 배열
+'''
 import sys
 num, cmax, ans = int(sys.stdin.readline()), 0, 0
 alist =[]
