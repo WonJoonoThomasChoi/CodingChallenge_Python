6e849bd386b8ced22e0a33f9cdc0d1838a0b6de4
WonJoonChoi <724thomas@gmail.com>
2023-01-11 07:56:16 +0900

    230111

--- LeetCode/LinkedList/234PalindromeLinkedList.py
+++ LeetCode/LinkedList/234PalindromeLinkedList.py
@@ -1,5 +1,13 @@
 # https://leetcode.com/problems/palindrome-linked-list/
-
+'''
+1. 아이디어 :
+    1) 배열을 하나 만들고, 노드를 순회하며 값을 넣은 후, 배열을 뒤집어서 비교한다.
+2. 시간복잡도 :
+    1) O(n)
+    - while 문
+3. 자료구조 :
+    1) 배열
+'''
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
