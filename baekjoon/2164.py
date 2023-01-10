0581756f551bf47771428a22b89c3895e27bbdef
WonJoonChoi <724thomas@gmail.com>
2023-01-11 07:57:01 +0900

    230111

--- baekjoon/2164.py
+++ baekjoon/2164.py
@@ -1,5 +1,14 @@
 # https://www.acmicpc.net/problem/2164
 
+'''
+1. 아이디어 :
+    1) 큐를 이용한다.
+2. 시간복잡도 :
+    1) O(n)
+    - n개의 원소를 append(1)와 pop(1)
+3. 자료구조 :
+    1) 큐
+'''
 import sys
 from collections import deque
 n = int(sys.stdin.readline())
