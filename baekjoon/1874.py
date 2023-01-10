bebcd508506e66210fc72d6133ba3466a7ecff81
WonJoonChoi <724thomas@gmail.com>
2023-01-11 07:56:55 +0900

    230111

--- baekjoon/1874.py
+++ baekjoon/1874.py
@@ -1,5 +1,15 @@
 # https://www.acmicpc.net/problem/1874
 
+'''
+1. 아이디어 :
+    1) 스택을 이용한다.
+2. 시간복잡도 :
+    1) O(n)
+    - n개의 원소를 append(1)와 pop(1)
+3. 자료구조 :
+    1) 스택
+'''
+
 import sys
 
 n,  alist, pointer, ans = int(sys.stdin.readline()), [0], 1, ""
