94ef9f29b8409ecd85e0706eab0b5b5048382fee
WonJoonChoi <724thomas@gmail.com>
2023-01-11 07:57:23 +0900

    230111

--- ProgrammersSchool/기능개발.py
+++ ProgrammersSchool/기능개발.py
@@ -1,5 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42586
-
+'''
+1. 아이디어 :
+    1) math.ceil을 이용하여 남은 일수를 계산하며 current max에 따라 count를 증가시킨다.
+2. 시간복잡도 :
+    1) O(n)
+    - for문을 돌면서 조건문 확인
+3. 자료구조 :
+    1) 배열
+'''
 import math
 def solution(progresses, speeds):
     answer, cmax, count = [], math.ceil((100 - progresses[0])/speeds[0]), 1
