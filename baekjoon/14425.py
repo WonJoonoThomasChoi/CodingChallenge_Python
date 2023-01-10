58558168a22220b08976a8cc8b8b9b6b598c9e2b
WonJoonChoi <724thomas@gmail.com>
2023-01-11 07:56:47 +0900

    230111

--- baekjoon/14425.py
+++ baekjoon/14425.py
@@ -1,5 +1,15 @@
 # https://www.acmicpc.net/problem/14425
 
+'''
+1. 아이디어 :
+    1) set을 이용하여 입력값을 받고, 입력값을 하나씩 확인하면서 set에 있는지 확인한다.
+2. 시간복잡도 :
+    1) O(n) + O(m) = O(n)
+    - n개의 원소를 set에 넣고, m개의 원소를 확인(1)
+3. 자료구조 :
+    1) set
+'''
+
 import sys
 n ,m = map(int, sys.stdin.readline().split())
 base = set()
