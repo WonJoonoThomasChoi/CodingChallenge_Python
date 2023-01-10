552d3bd5a3c84855883c7ebd91ad0624414aad5f
WonJoonChoi <724thomas@gmail.com>
2023-01-11 07:57:20 +0900

    230111

--- baekjoon/25325.py
+++ baekjoon/25325.py
@@ -1,5 +1,13 @@
 # https://www.acmicpc.net/problem/25325
-
+'''
+1. 아이디어 :
+    1) 딕셔너리를 이용하여 인덱스를 하나씩 올린 후, sort한다
+2. 시간복잡도 :
+    1) O(n) + O(n) * O(n) + O(nlogn)= O(n^2)
+    - for문으로 n개의 원소의 값을 0으로 초기화 + for문x2 로 인덱스값 하나씩 증가 + 정렬
+3. 자료구조 :
+    1) 스택
+'''
 import sys
 n, alist = int(sys.stdin.readline()), list(sys.stdin.readline().split())
 adict = {}
