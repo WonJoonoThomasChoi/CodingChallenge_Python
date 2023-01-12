3d4ce08b076f3ad0a443374e97e1e15f855a9128
WonJoonChoi <724thomas@gmail.com>
2023-01-13 06:36:17 +0900

    230113

--- baekjoon/11726.py
+++ baekjoon/11726.py
@@ -1,5 +1,13 @@
 # https://www.acmicpc.net/problem/11726
 
+'''
+1. 아이디어 :
+    1) dp문제인것 같다. 6번째까지 그려보니 패턴이 보인다. 결국에 가짓수는 피보나치 수열이다.
+2. 시간복잡도 :
+    1) O(n)
+3. 자료구조 :
+    1) 배열
+'''
 import sys
 # nums = list(map(int, input().split()))
 n = int(sys.stdin.readline())
