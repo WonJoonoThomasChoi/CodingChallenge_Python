242cc889629c537ca15c12940dbd4c0e3c090555
WonJoonChoi <724thomas@gmail.com>
2023-01-13 02:49:01 +0900

    230113

--- baekjoon/2750.py
+++ baekjoon/2750.py
@@ -1,9 +1,5 @@
 # https://www.acmicpc.net/problem/2750
 
 import sys
-num_list = []
-for i in range( int(sys.stdin.readline())):
-    num_list.append(int(sys.stdin.readline()))
-num_list.sort()
-for i in num_list:
-    print(i)
+input = sys.stdin.readline
+print(*sorted([int(input()) for _ in range(int(input()))]))
